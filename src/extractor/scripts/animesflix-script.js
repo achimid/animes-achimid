c2ad349ef0d5c682853c6e7829b152e8216742bd
Lourran <achimid@hotmail.com>
2023-10-12 15:55:14 -0300

    ajustando script

--- src/extractor/scripts/animesflix-script.js
+++ src/extractor/scripts/animesflix-script.js
@@ -1,6 +1,6 @@
 function getPostInfo($episode) {
     const url = $episode.querySelector('a').href
-    const anime = $episode.querySelector('.entry-title').innerText
+    const anime = $episode.querySelector('.entry-title').innerText.indexOf('(') >= 0 ? $episode.querySelector('.entry-title').innerText.split('(')[1].split(')')[0] :  $episode.querySelector('.entry-title').innerText
     const episode = parseInt($episode.querySelector('.num-epi').innerText.split('x')[1].match(/\d+/g))
     const title = `${anime} - Episódio ${episode}`
     
